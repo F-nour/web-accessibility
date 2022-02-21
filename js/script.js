359437be1f2f59809e0bd87352828aebc897f868
Nour-eddine <fattoumne@gmail.com>
2022-02-22 00:12:51 +0100

    20220221

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggle(id) {
+    var state = document.getElementById(id).style.display;
+        if (state == 'block') {
+            document.getElementById(id).style.display = 'none';
+        } else {
+            document.getElementById(id).style.display = 'block';
+        }
+    }
